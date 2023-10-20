655c150f43e3f4e2c60222055d2feb94b7d2abc8
marcellinojg <c14200043@john.petra.ac.id>
2023-10-20 13:18:01 +0700

    remove internal form component

--- src/pages/external/Landing.tsx
+++ src/pages/external/Landing.tsx
@@ -11,10 +11,10 @@ const Landing = () => {
                     <img src="/images/logo-without-text.png" className="w-24" alt="Logo SIAPPPAK" />
                 </div>
                 <h1 className="font-bold md:text-4xl text-2xl">PUSAT PELAPORAN KESEJAHTERAAN MASYARAKAT</h1>
-                <p>Website ini diperuntukkan untuk pelaporan adanya masalah dalam masyarakat seperti kekerasan rumah tangga, penganiayaan, dan lain lain.</p>
+                <p className="text-lg">Website ini diperuntukkan untuk pelaporan adanya masalah dalam masyarakat seperti kekerasan rumah tangga, penganiayaan, dan lain lain.</p>
                 <PrimaryLink to={ROUTES.EXTERNAL.PELAPORAN}  className="floating-shadow-md hover:bg-white hover:text-primary transition duration-300">LAPOR KASUS</PrimaryLink>
             </div>
-            <img src="/images/homeImage.png" width={200} className="w-1/3 md:block hidden" alt="" />
+            <img src="/images/homeImage.png" width={200} className="w-5/12 md:block hidden" alt="" />
         </div>
     </div>
 }
