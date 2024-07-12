fb81dec5c78664bebb3d46326d37b66af69ac79d
sidipraptama <sidiaurelius@gmail.com>
2024-07-12 11:21:28 +0700

    feat: kecamatan filtered surabaya

--- src/api/kecamatan.ts
+++ src/api/kecamatan.ts
@@ -2,9 +2,17 @@ import { Kecamatan } from "../consts/kecamatan"
 import { CreateAxiosInstance, CreatePublicAxiosInstance } from "../helpers/createAxiosInstance"
 
 
+// export const getKecamatans = async () => {
+//     const instance = CreatePublicAxiosInstance()
+//     const res = await instance.get('/m-kecamatan')
+//     const kecamatans = res.data.data as Kecamatan[]
+//     return kecamatans
+// }
+
 export const getKecamatans = async () => {
-    const instance = CreatePublicAxiosInstance()
-    const res = await instance.get('/m-kecamatan')
-    const kecamatans = res.data.data as Kecamatan[]
-    return kecamatans
+    const instance = CreatePublicAxiosInstance();
+    const res = await instance.get('/m-kecamatan');
+    const kecamatans = res.data.data as Kecamatan[];
+    const filteredKecamatans = kecamatans.filter(kecamatan => kecamatan.id_kabupaten === 1);
+    return filteredKecamatans;
 }
